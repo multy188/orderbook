f4bb5106fc880332fa781228baf81acab977b568
Kofi Asamoah-Boadu <asamoahkofi@hotmail.com>
2021-10-06 16:25:51 -0500

    initial setup for order book

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+export const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Order Book
+      </header>
+    </div>
+  );
+}
+
+export default App;
